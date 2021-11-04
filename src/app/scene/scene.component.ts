6f2e6abce69027abb4fc6d1d7e5cc8a585951085
AndreyRogachevich <andrey.rogachevich@azati.com>
2021-11-04 19:39:38 +0300

    two objects

--- src/app/scene/scene.component.ts
+++ src/app/scene/scene.component.ts
@@ -1,5 +1,12 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Input, OnInit, ViewChild } from '@angular/core';
 import * as THREE from 'three';
+
+export enum KEY_CODE {
+  RIGHT_ARROW = 'ArrowRight',
+  LEFT_ARROW = 'ArrowLeft',
+  UP_ARROW = 'ArrowUp',
+  DOWN_ARROW = 'ArrowDown',
+}
 
 @Component({
   selector: 'app-scene',
@@ -9,21 +16,19 @@ import * as THREE from 'three';
 export class SceneComponent implements AfterViewInit, OnInit {
   @ViewChild('canvas') canvasRef: ElementRef | undefined;
   @Input() cameraZ: number = 400;
-  @Input() fieldOfView: number = 1;
+  @Input() fieldOfView: number = 10;
   @Input('nearClipping') nearClippingPlane: number = 1;
-  @Input('farClipping') farClippingPlane: number = 1000;
-
+  @Input('farClipping') farClippingPlane: number = 10000;
+  step = 0.3;
   rotationSpeedX: number = 0.1;
   rotationSpeedY: number = 0.01;
-  size: number = 200;
-  texture: string = '/assets/45634.png';
-
+  pokeBallTexture: string = '/assets/45634.png';
+  texture: string = '/assets/texture.jpg';
   camera!: THREE.PerspectiveCamera;
   loader = new THREE.TextureLoader();
   geometry = new THREE.SphereGeometry();
-  material!: THREE.MeshBasicMaterial;
+  geometry2 = new THREE.BoxGeometry(2, 2, 2);
   mesh: THREE.Mesh[] = [];
-
   renderer!: THREE.WebGLRenderer;
   scene!: THREE.Scene;
 
@@ -34,9 +39,41 @@ export class SceneComponent implements AfterViewInit, OnInit {
     return this.canvasRef?.nativeElement;
   }
 
+  @HostListener('window:keydown', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    event.preventDefault();
+    if (event.key === KEY_CODE.UP_ARROW) {
+      this.camera.translateY(this.step);
+    }
+
+    if (event.key === KEY_CODE.DOWN_ARROW) {
+      this.camera.translateY(-this.step);
+    }
+    if (event.key === KEY_CODE.LEFT_ARROW) {
+      this.camera.translateX(this.step);
+    }
+
+    if (event.key === KEY_CODE.RIGHT_ARROW) {
+      this.camera.translateX(-this.step);
+    }
+  }
+
+  @HostListener('wheel', ['$event'])
+  wheelEvent(event: WheelEvent) {
+    event.preventDefault();
+    if (event.deltaY < 0) {
+      this.camera.translateZ(-10);
+      return;
+    }
+    this.camera.translateZ(10);
+
+  }
+
   ngOnInit(): void {
-    this.material = new THREE.MeshBasicMaterial({ map: this.loader.load(this.texture) });
-    this.mesh.push(new THREE.Mesh(this.geometry, this.material));
+    const pokeBallMaterial = new THREE.MeshLambertMaterial({ map: this.loader.load(this.pokeBallTexture) });
+    const material = new THREE.MeshLambertMaterial({ map: this.loader.load(this.texture) });
+    this.mesh.push(new THREE.Mesh(this.geometry, pokeBallMaterial));
+    this.mesh.push(new THREE.Mesh(this.geometry2, material));
   }
 
   ngAfterViewInit(): void {
@@ -49,9 +86,20 @@ export class SceneComponent implements AfterViewInit, OnInit {
   }
 
   createScene() {
+    // light on camera
+    const cameraPointLight = new THREE.PointLight(0xffffff);
+    cameraPointLight.position.set(1, 1, 2);
+
     // scene
     this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color('green');
+    this.scene.background = new THREE.Color('black');
+    // if (this.mesh[0]) {
+    //   this.mesh[0].position.set(0, 0, 0);
+    // }
+    if (this.mesh[1]) {
+      this.mesh[1].position.x = -5;
+      this.mesh[1].position.y = 5;
+    }
     this.scene.add(...this.mesh);
 
     // camera
@@ -62,15 +110,18 @@ export class SceneComponent implements AfterViewInit, OnInit {
       this.nearClippingPlane,
       this.farClippingPlane,
     );
+    this.camera.add(cameraPointLight);
     this.camera.position.z = this.cameraZ;
+
+    this.scene.add(this.camera);
   }
 
-  animateCube() {
-    if (!this.mesh[0]) {
+  animateCube(index: number) {
+    if (!this.mesh[index]) {
       return;
     }
-    this.mesh[0].rotation.x += this.rotationSpeedX;
-    this.mesh[0].rotation.y += this.rotationSpeedY;
+    this.mesh[index].rotation.x += this.rotationSpeedX;
+    this.mesh[index].rotation.y += this.rotationSpeedY;
   }
 
   startRenderingLoop() {
@@ -81,7 +132,8 @@ export class SceneComponent implements AfterViewInit, OnInit {
     let component: SceneComponent = this;
     (function render() {
       requestAnimationFrame(render);
-      component.animateCube();
+      component.animateCube(0);
+      component.animateCube(1);
       component.renderer.render(component.scene, component.camera);
     }());
   }
